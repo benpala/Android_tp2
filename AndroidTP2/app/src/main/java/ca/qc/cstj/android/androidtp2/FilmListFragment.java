e590a2fd598a9d95618bc9b2a77e788124460a79
benpala <gbenpala@gmail.com>
2015-11-06 15:31:31 -0500

    chose

--- AndroidTP2/app/src/main/java/ca/qc/cstj/android/androidtp2/FilmListFragment.java
+++ AndroidTP2/app/src/main/java/ca/qc/cstj/android/androidtp2/FilmListFragment.java
@@ -9,6 +9,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.google.gson.JsonArray;
 import com.koushikdutta.async.future.FutureCallback;
@@ -99,16 +100,6 @@ public class FilmListFragment extends ListFragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFilmFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFilmFragmentInteraction(String href);
